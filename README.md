2f1e704c69807262d2890b48d58b72a60bef8808
zwinger <zwinger55@yahoo.com>
2020-01-30 21:48:29 -0800

    Added clean flag and option to download only new images

--- README.md
+++ README.md
@@ -4,26 +4,28 @@
 ```
 python3
 PIL >= 7.0.0
-opencv-python 4.1.2
+opencv-python >= 4.1.2
 ```
 
-## dataExtractor.py
-Functionality: Download the images and extract mask information from the given .csv file if the optional -d flag is given. If the flag is ommited, do nothing.
+##dataExtractor.py
+Functionality: Download the images and extract mask information from the given .csv file. If the optional -d flag is given, re-download all the images in the file.
 ```
-Usage: python3 dataExtractor.py <filename.csv> [-d]
+Usage: python3 dataExtractor.py [-c | -a <filename.csv> | -n <filename.csv>]
 ``` 
 
-## Command Line Arguments
+##Command Line Arguments
 * filename.csv: The name of the .csv file to download and extract image and mask data from
-* -d: An optional flag argument to re-download the images from the given .csv file
+* -c: An optional flag argument to remove all the directories and files containing image data, a way to 'clean' all directory
+* -a: An optional flag argument to re-download all of the images from the given .csv file
+* -n: An optional flag argument to skip already downloaded images and their associated data and download any new images and their associated data
 
-## Directories
+##Directories
 The images and their mask data gathered from the dataExtractor.py script are stored in the following directories and .txt files. All directories and files are stored relative the path where the dataExtractor.py script was called from.
-* **Input_Images**: Stores a copy of the original image to be used for input.
-* **Image_Masks**: Stores a copy of all the masks associated with each of the input images.
-* **Mask_data**: Stores .txt files for all of the image masks with 128 points extracted from the respective image's mask stored in x,y column format. Each x,y pair is normalized to between 0 and 1.
-* **Mask_Validation**: Stores a copy of each input image with the extracted 128 points from the image's mask overlayed as green circles to validate the correct data is extracted from the mask.
-* **Whitelist_Masks**: Stores all of the image masks for images that have been whitelisted to validate no image will be used whose original mask was made incorrectly.
-* **Blacklist_Masks**: Stores all the image masks for images that have been blacklisted to validate that ther are no images whose mask was made correctly will not used.
-* **Whitelist_Images.txt**: Stores the names of all the images that have been whitelisted to be used as input.
-* **Blacklist_Images.txt**: Stores the names of all the images that have been blacklisted to not be used as input because they might have a labeling error.
+* Input_Images: Stores a copy of the original image to be used for input.
+* Image_Masks: Stores a copy of all the masks associated with each of the input images.
+* Mask_data: Stores .txt files for all of the image masks with 128 points extracted from the respective image's mask stored in x,y column format. Each x,y pair is normalized to between 0 and 1.
+* Mask_Validation: Stores a copy of each input image with the extracted 128 points from the image's mask overlayed as green circles to validate the correct data is extracted from the mask.
+* Whitelist_Masks: Stores all of the image masks for images that have been whitelisted to validate no image will be used whose original mask was made incorrectly.
+* Blacklist_Masks: Stores all the image masks for images that have been blacklisted to validate that ther are no images whose mask was made correctly will not used.
+* Whitelist_Images.txt: Stores the names of all the images that have been whitelisted to be used as input.
+* Blacklist_Images.txt: Stores the names of all the images that have been blacklisted to not be used as input because they might have a labeling error.
--- dataExtractor.py
+++ dataExtractor.py
@@ -4,25 +4,64 @@ import csv
 import cv2
 import os
 import sys
+import shutil
 from PIL import Image  
 
 '''Extract input and expected output data from the csv file'''
 def main():
    numArgs = len(sys.argv)
-   if (numArgs < 2 or numArgs > 3):
-      print("Usage: python3 dataExtractor.py <filename.csv> [-d]")
-      return
 
    args = sys.argv
-   if (numArgs == 3):
-      if (args[2] != "-d"):
-         print("Unknown flag: " + args[2])
-         print("Usage: python3 dataExtractor.py <filename.csv> [-d]")
+   if (numArgs == 2):
+      if (args[1] == '-c'):
+         cleanData()
          return
-      downloadImageData(args[1]);
+   if (numArgs == 3):
+      if (args[1] == '-a'):
+         downloadImageData(args[2], '-a');
+         return
+      elif (args[1] == '-n'):
+         downloadImageData(args[2], '-n');
+         return
+
+   print("Usage: python3 dataExtractor.py [-c | -a <filename.csv> | -n <filename.csv>]")
+   return
+
+'''Remove all the directories and files containing data information'''
+def cleanData():
+   confirm = "None"
+   while (confirm.lower() != 'y' and confirm.lower() != 'n'):
+      confirm = input("Are you sure you want to delete all image directories and data? (y/n): ")  
+   if (confirm == 'n'):
+      return
+
+   dirPath = os.getcwd() #Get the current directory path
+
+   try:
+      if os.path.isdir(dirPath + '/Input_Images'):
+         shutil.rmtree(dirPath + '/Input_Images')
+      if os.path.isdir(dirPath + '/Image_Masks'):
+         shutil.rmtree(dirPath + '/Image_Masks')
+      if os.path.isdir(dirPath + '/Mask_Data'):
+         shutil.rmtree(dirPath + '/Mask_Data')
+      if os.path.isdir(dirPath + '/Mask_Validation'):
+         shutil.rmtree(dirPath + '/Mask_Validation')
+      if os.path.isdir(dirPath + '/Blacklist_Masks'):
+         shutil.rmtree(dirPath + '/Blacklist_Masks')
+      if os.path.isdir(dirPath + '/Whitelist_Masks'):
+         shutil.rmtree(dirPath + '/Whitelist_Masks')
+      if os.path.isfile(dirPath + '/Whitelisted_Images.txt'):
+         os.remove(dirPath + '/Whitelisted_Images.txt')
+      if os.path.isfile(dirPath + '/Blacklisted_Images.txt'):
+         os.remove(dirPath + '/Blacklisted_Images.txt')
+   except OSError as err:
+      print("Error: {0}".format(err))
+      return
+
+      
 
 '''Download the image and mask data from the .csv file'''
-def downloadImageData(csvFile):
+def downloadImageData(csvFile, flag):
    try:
       imageFile = open(csvFile, 'r') #Open the csv file
    except:
@@ -31,8 +70,14 @@ def downloadImageData(csvFile):
 
    reader = csv.DictReader(imageFile)
    try:
-      whiteList = open("Whitelisted_Images.txt", 'w')
-      blackList = open("Blacklisted_Images.txt", 'w')
+      if (flag == '-a'):
+         whiteList = open("Whitelisted_Images.txt", 'w')
+         blackList = open("Blacklisted_Images.txt", 'w')
+      elif (flag == '-n'):
+         whiteList = open("Whitelisted_Images.txt", 'a')
+         blackList = open("Blacklisted_Images.txt", 'a')
+      else:
+         return
    except OSError as err:
       print("Error: {0}".format(err))
       return
@@ -57,13 +102,20 @@ def downloadImageData(csvFile):
       print("Error: {0}".format(err))
       return
 
-   imgNum = 1
+   imgNum = 0
    for row in reader:
+      imgNum += 1
       print("Image: " + str(imgNum), end = '')
+
+      '''Check if current image is already downloaded and only new images 
+         need to be download. If it exists, continue to the next image'''
+      if (flag == '-n' and os.path.isfile(dirPath + "/Input_Images/" + row['ID'] + ".jpg")):
+         print(" Skipping Image")
+         continue
+
       #Download the original image
       print(" Getting Original, ", end = '')
       imgUrl = row['Labeled Data']
-      #orgImg = urllib.request.urlretrieve(imgUrl)  #Retrieve the original image
       orgImg = getImageFromURL(imgUrl) #Retrieve the original image
       newImg = Image.open(orgImg[0])
       newImg = newImg.convert("RGB")   #Convert the image to RGB format
@@ -74,7 +126,6 @@ def downloadImageData(csvFile):
       #Download the mask for the image
       print("Getting Mask")
       maskUrl = row['Masks'].split('\"')[3]
-      #orgMask = urllib.request.urlretrieve(maskUrl) #Retrieve the original mask
       orgMask = getImageFromURL(maskUrl) #Retrieve the original mask
       newMask = Image.open(orgMask[0])
       newMask = newMask.convert('L')   #Convert the mask to grayscale format
@@ -125,8 +176,6 @@ def downloadImageData(csvFile):
 
       maskDataFile.close()
       newMask.close()
-
-      imgNum += 1
 
    imageFile.close()
    whiteList.close()
