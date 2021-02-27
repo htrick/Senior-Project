51c83745d417f03cdc44a760a38e34bd05bc5333
Hunter Trick <htrick@calpoly.edu>
2021-02-27 14:21:24 -0700

    Create basic inference script to test EfficientNet model

---
+++ EfficientNetB0/inference.py
@@ -0,0 +1,26 @@
+import efficientnetb0_pretrained
+import os
+import numpy as np
+import cv2
+
+def main(imageHeight, imageWidth, numOutputs, inputPath, outputPath):
+    model = efficientnetb0_pretrained.EfficientNetB0_Pretrained(shape=(360,640,3), num_outputs=128)
+
+    for file in os.listdir(inputPath):
+        imagePath = os.path.join(inputPath, file)
+
+        X = np.empty((1, imageHeight, imageWidth, 3), dtype='float32')
+        X[0, :, :, :] = cv2.imread(imagePath).copy()[:, :, ::-1] / 255.0
+
+        prediction = model.predict(X)[0]
+
+        validationMaskImage = cv2.imread(imagePath)
+        x = 0
+        for i in range(len(prediction)):
+            y = int(prediction[i] * imageHeight)
+            validationMaskImage = cv2.circle(validationMaskImage, (x, y), 1, (0, 255, 0), -1)
+            x += imageWidth // numOutputs
+        cv2.imwrite('{}/{}_inference.jpg'.format(outputPath, file), validationMaskImage)
+
+if __name__ == '__main__':
+    main(360, 640, 128, '../Input_Images', 'Inference_Images')
